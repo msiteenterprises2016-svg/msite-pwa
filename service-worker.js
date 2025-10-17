d4bb9f013783905857ab262d17c6137b934d8960
msiteenterprises2016-svg <msite.enterprises2016@gmail.com>
2025-10-17 17:21:09 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,11 @@
-const CACHE_NAME = "msite-cache-v1";
+const CACHE_NAME = "msite-cache-v2";
 const urlsToCache = [
   "./index.html",
   "./manifest.json",
   "./msite.png"
 ];
 
+// Install
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
@@ -12,27 +13,18 @@ self.addEventListener("install", event => {
   self.skipWaiting();
 });
 
+// Fetch
 self.addEventListener("fetch", event => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      fetch(event.request).catch(() => caches.match('./index.html'))
-    );
-    return;
-  }
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request).then(resp=>{
-        if (event.request.method === 'GET' && resp && resp.status===200) {
-          caches.open(CACHE_NAME).then(cache=>cache.put(event.request, resp.clone()));
-        }
-        return resp;
-      }).catch(()=>response);
-    })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
 
-self.addEventListener('activate', event => {
+// Activate (clear old caches)
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
   );
 });
